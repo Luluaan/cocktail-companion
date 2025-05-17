203b5d4ea8d4a154b0bb9985cb0c5f3160c19280
Villarejo Maxime <maxime.villarejo+smc@swissmedic.ch>
2025-05-17 14:44:52 +0200

    fix listener

--- feedback-service/src/main/java/ch/hearc/jee/feedbackservice/Listener/FeedbackListener.java
+++ feedback-service/src/main/java/ch/hearc/jee/feedbackservice/Listener/FeedbackListener.java
@@ -3,11 +3,16 @@ package ch.hearc.jee.feedbackservice.Listener;
 import ch.hearc.jee.feedbackservice.Respository.FeedbackRepository;
 import ch.hearc.jee.feedbackservice.model.Feedback;
 import ch.hearc.jee.feedbackservice.model.JmsMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeedbackListener {
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private final FeedbackRepository repo;
 
@@ -16,7 +21,9 @@ public class FeedbackListener {
     }
 
     @JmsListener(destination = "feedback-queue")
-    public void onFeedbackMessage(JmsMessage jmsMessage) {
+    public void onFeedbackMessage(String message) throws JsonProcessingException {
+        JmsMessage jmsMessage = objectMapper.readValue(message, JmsMessage.class);
+        System.out.println("Message reçu : " + jmsMessage.getComment());
         Feedback feedback = new Feedback();
         feedback.setCocktailId(jmsMessage.getIdDrink());
         feedback.setComment(jmsMessage.getComment());
